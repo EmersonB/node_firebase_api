b9cb4b49a7440830a0ed2effefaf070dad68891d
EmersonB <eberlik@gmail.com>
2018-03-10 16:58:10 -0500

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var firebase = require("firebase");
 var router = express.Router();
 var app = express();
 
+const gm_api_key = "AIzaSyDKxDdzzYAtOJDb-rgiJIRJy-w-Fcr1wOM";
+
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/'));
@@ -18,18 +20,18 @@ var listener = app.listen(app.get('port'), function() {
 });
 
 firebase.initializeApp({
-  databaseURL: "https://fir-3fd40.firebaseio.com",
+  databaseURL: "https://hacktj2018.firebaseio.com",
   service_account: "service.json"
 })
 
 var db = firebase.database();
-var ref = db.ref("employees");
+var ref = db.ref("ids");
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname+'/index.html');
 });
 
-router.route('/employees')
+router.route('/ids')
   .get(function(req,res){
     ref.on("value", function(snapshot) {
       console.log(snapshot.val());
@@ -39,9 +41,9 @@ router.route('/employees')
     });
   });
 
-router.route('/employee/:name')
+router.route('/:device_id/route')
   .get(function(req,res){
-    ref.child(req.params.name).on("value", function(snapshot) {
+    ref.child(req.params.device_id).child("route").on("value", function(snapshot) {
       console.log(snapshot.val());
       res.json(snapshot.val());
     }, function (errorObject) {
@@ -49,17 +51,42 @@ router.route('/employee/:name')
     });
   })
   .post(function(req,res){
-    var usersRef = ref.child(req.params.name);
+    var usersRef = ref.child(req.params.device_id).child("route");
     usersRef.set({
-      name: req.params.name,
-      age: req.body.age,
-      position: req.body.position
+      start_lat: req.body.start.lat,
+      start_long: req.body.start.long,
+      end_lat: req.body.end.lat,
+      end_long: req.body.end.long
     });
   })
   .put(function(req, res){
 
   })
   .delete(function(req,res){
-    var usersRef = ref.child(req.params.name);
-    usersRef.set(null);
+    // var usersRef = ref.child(req.params.name);
+    // usersRef.set(null);
   });
+
+  router.route('/:device_id/location')
+    .get(function(req,res){
+      ref.child(req.params.device_id).child("location").on("value", function(snapshot) {
+        console.log(snapshot.val());
+        res.json(snapshot.val());
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+    })
+    .post(function(req,res){
+      var usersRef = ref.child(req.params.device_id).child("location");
+      usersRef.set({
+          lat: req.body.lat,
+          long: req.body.long
+      });
+    })
+    .put(function(req, res){
+
+    })
+    .delete(function(req,res){
+      // var usersRef = ref.child(req.params.name);
+      // usersRef.set(null);
+    });
